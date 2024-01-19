260905dbf910d9d0d6c4bcc644840f43fd90bd94
palak <palakchaudhari311@gmail.com>
2024-01-19 13:30:33 -0500

    Just printed Java Git.

---
+++ src/java_github/Java_Github.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_github;
+
+/**
+ *
+ * @author palak
+ */
+public class Java_Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Java Git");
+    }
+    
+}
